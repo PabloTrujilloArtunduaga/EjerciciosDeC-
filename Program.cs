a83848dac5bc8e3cba4a447007c377a08e9c1037
Pablo Tujillo Artunduaga <pabloartunduaga2020@gmail.com>
2026-07-24 20:41:15 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,102 +4,104 @@
     {
         static void Main(string[] args)
         {
-           // Ejercicio Calculadora
+           // Ejercicio Gestor de Tareas Pendientes (To-Do List)
+           List<Tarea> tareaLista = new List<Tarea>();           
+           int contadorId = 1;
            int option = 0;
 
+
            while(option != 5)
-           {
-              Console.WriteLine("Escribir operacion a realizar (1- sumar, 2-restar, 3- dividir, 4- multiplicar, 5- salir) ");
-              option = Convert.ToInt32(Console.ReadLine());
+            {
+               
+                
 
-
-                if (option == 5)
-                {
-                    Console.WriteLine("Saliste");
-                    break;
-                    
-                }
-
-               if (option < 1 || option > 5)
-                {
-                    Console.WriteLine("Opcion invalida");
-                    continue;
-
-                }
-
-              
-              Console.WriteLine("Escribir numero 1: ");
-              int number1 = Convert.ToInt32(Console.ReadLine());
-              
-              Console.WriteLine("Escribir numero 2: ");
-              int number2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Acción a realizar: (1- Crear, 2- Listar, 3- Borrar, 4- Actualizar, 5- Salir)");
+                option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
                 {
                     case 1:
-                        Console.WriteLine(addNumbers(number1, number2));
+                        Console.WriteLine("Crear descripción de la tarea.");
+                        Console.WriteLine("Escribir la descripción de la tarea: ");
+                        String descripcion = Console.ReadLine() ?? "";
+                        Console.WriteLine("Escribir el estado de la tarea: ");
+                        String estadoTarea = Console.ReadLine() ?? "";
+                        Tarea nuevaTarea = new Tarea(contadorId, descripcion, estadoTarea);
+                        tareaLista.Add(nuevaTarea);
+                        contadorId++;
                         break;
-
                     case 2:
-                        Console.WriteLine(substractNumbers(number1, number2));
+                        Console.WriteLine("Listar tareas.");
+                        foreach(Tarea t in tareaLista)
+                        {
+                            Console.WriteLine($"ID: {t.Id} | Tarea: {t.Descripcion} | Estado: {t.Estado}");
+                        }
                         break;
-
                     case 3:
-                        Console.WriteLine(divideNumbers(number1, number2));
+                        Console.WriteLine("Eliminar tarea.");
+                        Console.WriteLine("Eliminar por ID: ");
+                        int idAEliminar = Convert.ToInt32(Console.ReadLine());
+                        Tarea tareaAEliminar = tareaLista.Find(t => t.Id == idAEliminar);
+                            if(tareaAEliminar != null)
+                            {
+                                tareaLista.Remove(tareaAEliminar);
+                                Console.WriteLine($"La tarea con ID {idAEliminar} fue eliminada con éxito.");
+                            } else
+                            {
+                                Console.WriteLine("No se encontró ese ID.");
+                            }
+                        
+                        break;
+                    case 4:
+                        Console.WriteLine("Actualizar tarea.");
+                        Console.WriteLine("Seleccionar ID: ");
+                        int idAactualizar = Convert.ToInt32(Console.ReadLine());
+                        Tarea tareaActualizar = tareaLista.Find(t => t.Id == idAactualizar);
+                         if(tareaActualizar != null)
+                            {
+                                Console.WriteLine("Escribir la descripción de la tarea: ");
+                                String nuevaDescripcion = Console.ReadLine() ?? "";
+                                Console.WriteLine("Escribir el estado de la tarea: ");
+                                String NuevoEstadoTarea = Console.ReadLine() ?? "";
+                                tareaActualizar.Descripcion = nuevaDescripcion;
+                                tareaActualizar.Estado = NuevoEstadoTarea;
+                                Console.WriteLine("Tarea actualizada.");
+                                
+                            } else
+                            {
+                                Console.WriteLine("No se encontró ese ID.");
+                            }
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliste.");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida.");
                         break;
 
-                    case 4:
-                        Console.WriteLine(multyNumbers(number1, number2));
-                        break;       
+                        
                 }
-
-               
+                
             }
-
-            Console.ReadKey(); 
         }
 
-        public static int addNumbers(int a, int b)
+    }
+
+
+
+    public class Tarea
+    {
+    public int Id { get; private set; }
+    public string Descripcion { get; set; }
+    public string Estado { get; set; }
+
+    
+        public Tarea(int id, string descripcion, string estado)
         {
-            int c = a + b;
-            Console.WriteLine("La suma es de: ");
-            return c;
-            
+            Id = id;
+            Descripcion = descripcion;
+            Estado = estado; 
         }
-
-         public static int substractNumbers(int a, int b)
-        {
-            int c = a - b;
-            Console.WriteLine("La resta es de: ");
-            return c;
-            
-        }
-
-         public static int divideNumbers(int a, int b)
-        {   
-            try
-            {
-                int c = a / b;
-                Console.WriteLine("La division es de: ");
-                return c;
-            } catch (DivideByZeroException)
-            {
-                Console.WriteLine("Error: No se puede dividir por cero.");
-                return 0;
-            }
-            
-            
-        }
-
-         public static int multyNumbers(int a, int b)
-        {
-            int c = a * b;
-            Console.WriteLine("La multplicacion es de ");
-            return c;
-            
-        }
-
-
-       
+  
     }
 }
